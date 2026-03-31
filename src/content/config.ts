2e534251adf1f90c5bad885ea2a672906731fc0f
Erick Briones <erickalan518@gmail.com>
2026-03-31 16:16:11 -0600

    Optimizando carga de imágenes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,22 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
-const blogCollection = defineCollection ({
-    type: 'content',
-    schema: z.object({
+const blogCollection = defineCollection({
+    type: "content",
+    // loader: glob({ base: './src/content/blog', pattern: "**/*.{md,mdx}"}),
+    schema: ({ image }) => z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
+        image: image().refine(img => img, {
+            message: ""
+        }),
+
+        // Relación
         author: z.string(),
-        image: z.string(),
+
+        // Relación
         tags: z.array(z.string())
     })
 });
