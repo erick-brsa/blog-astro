86cc6cf208cfc56212ad1786a241d1efe03ba9c0
Erick Briones <erickalan518@gmail.com>
2026-03-31 22:12:06 -0600

    Mostrando la página del autor el post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,4 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection, reference, z } from 'astro:content';
 
 const blogCollection = defineCollection({
     type: "content",
@@ -14,13 +12,25 @@ const blogCollection = defineCollection({
         }),
 
         // Relación
-        author: z.string(),
+        author: reference("author"),
 
         // Relación
-        tags: z.array(z.string())
+        tags: z.array(z.string()),
+
+        // Boolean
+        isDraft: z.boolean().default(false),
     })
 });
 
+const authorCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image()
+    })
+})
+
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection
 }
